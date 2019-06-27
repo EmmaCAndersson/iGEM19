e3630dd9be4ed853f91be0b1c7ab69760ddabb6c
tiliasellden <52192854+tiliasellden@users.noreply.github.com>
2019-06-27 14:53:16 +0200

    Add files via upload

--- Code/Reactor/main1_reactor_simulation.m
+++ Code/Reactor/main1_reactor_simulation.m
@@ -1,31 +1,51 @@
 % This is the main progam file
 % CSTR simulation: Batch simulation better?
-
-% The following variables must be defined
-...
-Qin =1
-Qout =0.2 
-
-% The federal government has established the safe drinking water standard (also called maximum
-% contaminant level) for PCBs as 500 ppt (parts per trillion)
-MCL=500*10^-12
-
-Cin = 4.4     % Median concentration accoring to Naturvårdsverket is 4.4 microg summa-PCB7/kg TS
-V = 1         % If the volume is constant set V=1
-R = 0.5       % Rate of reaction (if PCB rate of degradation) 
-y0=20         % Biomass conc. at t0 
-
-% Mass balance for the reactor: Concentration over time equals the (inflow -
-% the outflow) + the production/degradation
-
 % We want to solve the mass balance equ. and get C_out to be at "maximun
 % contaminant level" 
 
-tspan=[0 1000];
+%% Varaible dictionary
+% Name    Type           Unit    Description
+% ca,cb                  mol/L   concentration
+% t                      h       time
+% k                      1/h     rate of speed
+% r                      mol/l*h velocity
+% Qin
+% Qout
+% MCL                             The federal government has established the safe drinking water standard (also called maximum contaminant level) for PCBs as 500 ppt (parts per trillion)
+% Cin                             Median concentration accoring to Naturvårdsverket is 4.4 microg summa-PCB7/kg TS
+% V                               If the volume is constant set V=1
+% R                               Rate of reaction (if PCB rate of degradation) 
+% Y0                              Biomass concentration at t0 
 
-[t,y]=ode15s(@(t,Y)massbalance1(t,Y,Qin,Qout,Cin,Cout,V,R), tspan, y0)
+
+%% Massbalance calculation (CSTR)
+t=[0 1000];
+
+[t,y]=ode15s(@(t,Y)massbalance1,t,y)
 
 plot(t,y,'-o')
 
+%% Kinetic reaction calculation (fed batch)
+% The rection from PCB to benzoate and hydroxypenta-dienote involves 6
+% enzymekatalysed reactions
+% PCB -> 
+clear
+clc
+close all
+
+ca0	= 5; %mol l^-1 
+cb0 = 0; % mol/L
+
+t   = 0:0.1:10;
+
+Y0  = [ca0 cb0];
+
+%kalla på funktion tex ODE ösare
+[t,Y] = ode15s(@kinetic_reak,t,Y0)
+
+%plotta resultat
+
+plot(t,Y)
+legend({'A','B'},'Location','best')
 
 
--- Code/Reactor/massbalance1.m
+++ Code/Reactor/massbalance1.m
@@ -1,4 +1,21 @@
 function dydt=massbalance1(t,y,Qin,Qout,Cin,Cout,V,R)
+% The following variables must be defined
+...
+Qin =1
+Qout =0.2 
+
+% The federal government has established the safe drinking water standard (also called maximum
+% contaminant level) for PCBs as 500 ppt (parts per trillion)
+MCL=500*10^-12
+
+Cin = 4.4
+V = 1         
+R = 0.5       
+y0=20         
+
+% Mass balance for the reactor: Concentration over time equals the (inflow -
+% the outflow) + the production/degradation
+
 
 % This calculate the mass balance in the reactor 
 
